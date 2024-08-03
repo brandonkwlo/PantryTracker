bf5578ce5762c926409e5aa7ba7b22e8a4cedb71
brandonkwlo <brandonkwlo@gmail.com>
2024-08-03 13:07:45 -0700

    modified firebase.js for vercel

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { firebaseApiKey } from "/.env.local";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: firebaseApiKey,
+  apiKey: process.env.firebaseApiKey,
   authDomain: "headstarterpantry.firebaseapp.com",
   projectId: "headstarterpantry",
   storageBucket: "headstarterpantry.appspot.com",
